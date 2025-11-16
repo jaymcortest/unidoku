a13d00add11d5d694d1e2b8149b00e0257cdcf8a
jaymcortest <jaymcortest@gmail.com>
2025-11-16 08:19:12 +0000

    Deploying to gh-pages from @ jaymcortest/unidoku@401dcbb13a3e140f12b813ae020611fea211eabf 🚀

---
+++ unidoku_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_sudoku_free: (a: number, b: number) => void;
+export const main: () => void;
+export const sudoku_check_solution: (a: number) => void;
+export const sudoku_new: () => number;
+export const sudoku_new_game: (a: number, b: any) => void;
+export const sudoku_render: (a: number, b: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h3609c48714a532a2: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__closure__destroy__h0a77b1b88e11d55e: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
